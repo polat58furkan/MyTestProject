93cf0ce1ddbf1bde08e602cd6a59e0d9d8c532d6
Furkan Polat <polat58furkan@gmail.com>
2025-12-02 22:23:29 +0300

    Square Homework

--- MyTestPy.py
+++ MyTestPy.py
@@ -1,1 +1,15 @@
 #First test
+import turtle
+
+drawing_board = turtle.Screen()
+
+drawing_board.bgcolor("DarkMagenta")
+drawing_board.title("My Py")
+drawing_board.title("Hello World")
+turtle_instance = turtle.Turtle()
+
+for i in range(4):
+    turtle_instance.forward(100)
+    turtle_instance.left(90)
+turtle.done()
+
